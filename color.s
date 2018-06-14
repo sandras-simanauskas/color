55fcff160f56bbc5b9f14e2e8acfbd2b14ddac13
sandras-systems <sandras.simanauskas@protonmail.com>
2018-06-14 15:27:09 +0000

    Remove leading 0s

--- color.s
+++ color.s
@@ -1,9 +1,9 @@
 org	0x7C00
 
-mov	ax,		0x0013
+mov	ax,		0x13
 int	0x10
 
-start:	mov		dx,		0x03DA
+start:	mov		dx,		0x3DA
 
 wait0:	in		al,		dx
 	test		al,		8
@@ -14,21 +14,21 @@ wait1:	in		al,		dx
 	jz		wait1
 
 	xor		al,		al
-	mov		dx,		0x03C8
+	mov		dx,		0x3C8
 	out		dx,		al
 
-	mov		dx,		0x03C9
+	mov		dx,		0x3C9
 	mov		si,		color
 
 	mov 		ecx,		3
 rep	outsb
 
-	or		dword [color],	0x00C0C0C0
+	or		dword [color],	0xC0C0C0
 	inc		dword [color]
 
 	jmp		start
 
-color:			dd		0x00C0C0C0
+color:			dd		0xC0C0C0
 
 times	510-($-$$) 	db		0
 			dw		0xAA55
